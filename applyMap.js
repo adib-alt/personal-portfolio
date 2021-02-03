507c5953b0f20504bd0b3d3039520f969512397a
Adib-alt <farjanasume22@gmail.com>
2021-02-03 12:35:40 +0600

    portfolio

---
+++ applyMap.js
@@ -0,0 +1,11 @@
+const players = [
+
+{Name:'Tamim Iqbal', id:1},
+{Name:'Messi' , id:2},
+{Name:'ronaldo' ,id:3},
+{Name:'dybala' , id:4}
+]
+
+
+let name = players.map(p => p.Name);
+console.log(name);
